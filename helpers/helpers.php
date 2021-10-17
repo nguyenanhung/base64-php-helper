3e97062e61043eac0927b06b8e5c5cd67dfa89a6
nguyenanhung <info@nguyenanhung.com>
2021-10-18 00:34:30 +0700

    Release version 1.0.0

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,74 @@
 <?php
+/**
+ * Project base64-php-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/18/2021
+ * Time: 00:30
+ */
+if (!function_exists('base64url_encode')) {
+    /**
+     * Function encode
+     *
+     * @param      $data
+     * @param bool $usePadding
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/8/19 31:44
+     */
+    function base64url_encode($data, $usePadding = false)
+    {
+        return nguyenanhung\Libraries\Base64\Base64::base64UrlEncode($data, $usePadding);
+    }
+}
+if (!function_exists('base64url_decode')) {
+    /**
+     * Function base64url_decode
+     *
+     * @param $data
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 18:27
+     */
+    function base64url_decode($data)
+    {
+        return nguyenanhung\Libraries\Base64\Base64::base64UrlDecode($data);
+    }
+}
+if (!function_exists('super_base64_encode')) {
+    /**
+     * Function super_base64_encode
+     *
+     * @param $input
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 19:36
+     */
+    function super_base64_encode($input)
+    {
+        return nguyenanhung\Libraries\Base64\Base64::superBase64Encode($input);
+    }
+}
+if (!function_exists('super_base64_decode')) {
+    /**
+     * Function super_base64_decode
+     *
+     * @param $input
+     *
+     * @return bool|string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 20:11
+     */
+    function super_base64_decode($input)
+    {
+        return nguyenanhung\Libraries\Base64\Base64::superBase64Decode($input);
+    }
+}
---
+++ src/Base64.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Project base64-php-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/18/2021
+ * Time: 00:31
+ */
+
+namespace nguyenanhung\Libraries\Base64;
+
+use InvalidArgumentException;
+
+/**
+ * Class Base64
+ *
+ * @package   nguyenanhung\Libraries\Base64
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+class Base64
+{
+    /**
+     * Function superBase64Encode
+     *
+     * @param $input
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/18/2021 33:04
+     */
+    public static function superBase64Encode($input)
+    {
+        $output = $input;
+        $output = base64_encode($output);
+        $output = strrev($output);
+        $output = base64_encode($output);
+        $output = base64_encode($output);
+
+        return strrev($output);
+    }
+
+    /**
+     * Function superBase64Decode
+     *
+     * @param $input
+     *
+     * @return bool|string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/18/2021 32:59
+     */
+    public static function superBase64Decode($input)
+    {
+        $output = $input;
+        $output = strrev($output);
+        $output = base64_decode($output);
+        $output = base64_decode($output);
+        $output = strrev($output);
+
+        return base64_decode($output);
+    }
+
+    /**
+     * Function base64UrlEncode
+     *
+     * @param       $data
+     * @param bool  $usePadding
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/18/2021 32:52
+     */
+    public static function base64UrlEncode($data, $usePadding = false)
+    {
+        $encoded = strtr(base64_encode($data), '+/', '-_');
+
+        return true === $usePadding ? $encoded : rtrim($encoded, '=');
+    }
+
+    /**
+     * Function base64UrlDecode
+     *
+     * @param $data
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 10/18/2021 32:49
+     */
+    public static function base64UrlDecode($data)
+    {
+        $decoded = base64_decode(strtr($data, '-_', '+/'), true);
+        if (false === $decoded) {
+            throw new InvalidArgumentException('Invalid data provided');
+        }
+
+        return $decoded;
+    }
+}
